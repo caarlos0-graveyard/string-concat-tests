3b389ebf398957bd13869bff41b59e5465f92cdd
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-21 11:04:36 -0300

    Concatenating int in one test and String in another isn't fair. Correcting.

--- test2/Test2.java
+++ test2/Test2.java
@@ -2,7 +2,7 @@ public class Test2 {
   public static void main(String[] args) {
     StringBuilder out = new StringBuilder();
     for( int i = 0 ; i < 10000; i++ ) {
-        out.append(i);
+        out.append("1");
     }
     System.out.println(out.toString());
   }
